53e2f1e0feec42673f765c41c11d43f82a06a9da
ragnarokkr <893472+Ragnarokkr@users.noreply.github.com>
2025-08-01 12:15:32 +0200

    fix: sourcing path for relative config files

--- _config/05_dotfiles-setup.ps1
+++ _config/05_dotfiles-setup.ps1
@@ -1,12 +1,12 @@
-. .\05_01_windowsterminal.ps1
-. .\05_02_ohmyposh.ps1
-. .\05_03_powershell.ps1
-. .\05_04_helix.ps1
-. .\05_05_vscode.ps1
-. .\05_06_git.ps1
-. .\05_07_wallpapers.ps1
-. .\05_08_cursors.ps1
-. .\05_09_nerdfonts.ps1
+. (Join-Path $PSScriptRoot '05_01_windowsterminal.ps1')
+. (Join-Path $PSScriptRoot '05_02_ohmyposh.ps1')
+. (Join-Path $PSScriptRoot '05_03_powershell.ps1')
+. (Join-Path $PSScriptRoot '05_04_helix.ps1')
+. (Join-Path $PSScriptRoot '05_05_vscode.ps1')
+. (Join-Path $PSScriptRoot '05_06_git.ps1')
+. (Join-Path $PSScriptRoot '05_07_wallpapers.ps1')
+. (Join-Path $PSScriptRoot '05_08_cursors.ps1')
+. (Join-Path $PSScriptRoot '05_09_nerdfonts.ps1')
 
 # ---------------------------------------------------------------------------- #
 #                                     MAIN                                     #
